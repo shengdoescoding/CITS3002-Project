b67f0e9e7230f14ae7ad1d2c4f85bdacef2b9ace
papasheng <wassuprt@gmail.com>
2022-05-08 11:50:19 +0800

    Created data structre to hold infomation from rake file

--- rake_c/rake_client_c.c
+++ rake_c/rake_client_c.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define RAKE_FILE_DIR "../rakefiles/"
 #define MAX_LINE_LEN 128
+
+struct Rake_File{
+    int port;
+    char *hosts;
+    int total_actionsets;
+    struct Actionset *actset;
+} rake_file;
+struct Actionset{
+    int total_actions;
+    struct Action *act;
+};
+struct Action{
+    bool remote;
+    char *command;
+    int total_files;
+    char **required_files;
+};
+
 
 int main(int argc, char const *argv[])
 {
@@ -16,18 +35,18 @@ int main(int argc, char const *argv[])
         return EXIT_FAILURE;
     }
 
-    char *temp;
-    temp = malloc(strlen(argv[1]) * sizeof(char));
-    strcpy(temp, argv[1]);
+    char *temp_argv1;
+    temp_argv1 = malloc(strlen(argv[1]) * sizeof(char));
+    strcpy(temp_argv1, argv[1]);
     char *rake_file;
     rake_file = malloc((strlen(argv[1]) + strlen(RAKE_FILE_DIR)) * sizeof(char));
     if(rake_file == NULL){
-        fprintf(stderr, "Fatal: failed to allocate memory for rake_file_address.\n");
+        fprintf(stderr, "Fatal: failed to allocate memory for rake file address.\n");
         return EXIT_FAILURE;
     }
     strcat(rake_file, RAKE_FILE_DIR);
-    strcat(rake_file, temp);
-    free(temp);
+    strcat(rake_file, temp_argv1);
+    free(temp_argv1);
     // DEBUG FOR THIS SEGMENT
     printf("%s\n", rake_file);
     
@@ -41,31 +60,33 @@ int main(int argc, char const *argv[])
      * e.g. clear white spaces at the end of lines, clear empty lines etc.
      **/
 
-    char **rake_file_info;
     char line[MAX_LINE_LEN];
-    int indentation_size = 0;
     while (fgets(line, MAX_LINE_LEN, fp)){
         // Ignore comments
         if(line[0] == '#'){
             continue;
         }
 
+        if(strstr(line, "actionset") != NULL){
+            int actionset_number = line[9] - '0';
+            
+        }
+
         // Detect indentation
+        // Assume one indentation is 4 spaces!
         int temp_indentation_size = 0;
-        for(int i = 0; i < strlen(line); i++){
+        for(size_t i = 0; i < strlen(line); i++){
             if(line[i] == ' '){
                 temp_indentation_size++;
+                printf("%i\n", temp_indentation_size);
             }
             else{
                 break;
             }
         }
-        if(indentation_size == 0){
-            indentation_size = temp_indentation_size;
-        }
 
         // Do something with the lines knowing its indentation
-        if(temp_indentation_size == 0){
+        if(temp_indentation_size == 4){
             printf("%s\n", line);
 
         }
