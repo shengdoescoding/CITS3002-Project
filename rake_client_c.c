caed9959f10812b62c34c596acce03b86e85c4e0
papasheng <wassuprt@gmail.com>
2022-05-12 18:48:50 +0800

    Updated for support for multiple actionsets, not yet tested

--- rake_client_c.c
+++ rake_client_c.c
@@ -7,18 +7,22 @@
 
 #define RAKE_FILE_DIR "rakefiles/"
 #define MAX_LINE_LEN 128
+int CURR_ACTSET_INDEX;
+int CURR_ACT_INDEX;
 
 struct Rake_File{
     int port;
     int total_hosts;
     char **hosts;
     int total_actionsets;   // Determine at the end of the wile loop by -1 from the last actionset number
-    struct Actionset **actset;
+    struct Actionset **actsets;
 } rake_file;
+
 struct Actionset{
     int total_actions;
     struct Action **acts;
-} actionset;
+};
+
 struct Action{
     bool remote;
     char *command;
@@ -31,22 +35,19 @@ void init_rake_file(){
     rake_file.total_hosts = 0;
     rake_file.total_actionsets = 0;
     rake_file.hosts = NULL;
-    rake_file.actset = NULL;
+    rake_file.actsets = NULL;
 }
 
-void init_actionset(){
-    actionset.total_actions = 0;
-    actionset.acts = NULL;
+void init_actionset(struct Actionset *actionset){
+    actionset->total_actions = 0;
+    actionset->acts = NULL;
 }
 
-struct Action * init_action(){
-    struct Action *action;
-    action = malloc(sizeof(struct Action));
+void init_action(struct Action *action){
     action->remote = false;
     action->command = NULL;
     action->required_files = NULL;
     action->total_files = 0;
-    return action;
 }
 
 void mem_alloc_check(const void *p, char *var_name){
@@ -98,12 +99,21 @@ void add_command(struct Action *action, const char *line_no_whitespace){
     }
 }
 
+void add_actionset_to_rakefile(struct Actionset *actionset){
+    rake_file.actsets = realloc(rake_file.actsets, sizeof(struct Actionset *) * rake_file.total_actionsets);
+    mem_alloc_check(rake_file.actsets, "rake_file.actsets");
+    rake_file.actsets[CURR_ACTSET_INDEX] = malloc(sizeof(struct Actionset));
+    mem_alloc_check(rake_file.actsets[CURR_ACTSET_INDEX], "rake_file.actsets[CURR_ACTSET_INDEX]");
+    rake_file.actsets[CURR_ACTSET_INDEX] = actionset;
+}
+
 void add_action_to_actionset(struct Action *action){
-    actionset.acts = realloc(actionset.acts, sizeof(struct Action *) * actionset.total_actions);
-    mem_alloc_check(actionset.acts, "actuibset.acts");
-    actionset.acts[actionset.total_actions - 1] = malloc(sizeof(struct Action));
-    mem_alloc_check(actionset.acts[actionset.total_actions - 1], "actionset.acts[actionset.total_actions - 1]");
-    actionset.acts[actionset.total_actions - 1] = action;
+    rake_file.actsets[CURR_ACTSET_INDEX]->acts = realloc(rake_file.actsets[CURR_ACTSET_INDEX]->acts, sizeof(struct Action *) * rake_file.actsets[CURR_ACTSET_INDEX]->total_actions);
+    mem_alloc_check(rake_file.actsets[CURR_ACTSET_INDEX]->acts, "rake_file.actsets[CURR_ACTSET_INDEX]->acts");
+
+    rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX] = malloc(sizeof(struct Action));
+    mem_alloc_check(rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX], "rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]");
+    rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX] = action;
 }
 
 void add_hosts(const char *line){
@@ -121,7 +131,6 @@ void add_hosts(const char *line){
     }
     free_words(hosts);
 }
-
 int main(int argc, char const *argv[])
 {
     if(argc <= 1){
@@ -145,7 +154,6 @@ int main(int argc, char const *argv[])
     free(temp_argv1);
     
     init_rake_file();
-    init_actionset();
 
     FILE *fp = fopen(rake_file_address, "r");
     if (fp == NULL){
@@ -153,7 +161,6 @@ int main(int argc, char const *argv[])
         return EXIT_FAILURE;
     }
     char line[MAX_LINE_LEN];
-    int actionset_number = 0;
     while (fgets(line, MAX_LINE_LEN, fp)){
         // Detect indentation
         // Assume one indentation is 4 spaces!
@@ -186,13 +193,21 @@ int main(int argc, char const *argv[])
 
         // Detect actionset
         if(strstr(line, "actionset") != NULL && temp_indentation_size == 0){
-            actionset_number = line[9] - '0';
+            struct Actionset *actionset = malloc(sizeof(*actionset));
+            mem_alloc_check(actionset, "actionset");
+            init_actionset(actionset);
+            rake_file.total_actionsets++;
+            CURR_ACTSET_INDEX = rake_file.total_actionsets - 1;
+            add_actionset_to_rakefile(actionset);
         }
 
         // Detect command
         if(temp_indentation_size == 4){
-            struct Action *action = init_action();        
-            actionset.total_actions++;
+            struct Action *action = malloc(sizeof(*action));
+            mem_alloc_check(action, "action");
+            init_action(action);
+            rake_file.actsets[CURR_ACTSET_INDEX]->total_actions++;
+            CURR_ACT_INDEX = rake_file.actsets[CURR_ACTSET_INDEX]->total_actions - 1;
             add_command(action, line_no_whitespace);
             // Store into actionset
             add_action_to_actionset(action);
@@ -202,15 +217,15 @@ int main(int argc, char const *argv[])
             // Populate action in actionset with required files
             int nwords;
             char **required_files = strsplit(line, &nwords);
-            actionset.acts[actionset.total_actions - 1]->total_files = nwords - 1;
-            actionset.acts[actionset.total_actions - 1]->required_files = malloc(actionset.acts[actionset.total_actions - 1]->total_files * sizeof(char*));
-            mem_alloc_check(actionset.acts[actionset.total_actions - 1]->required_files, "actionset.acts[actionset.total_actions - 1]->required_files");
+            rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->total_files = nwords - 1;
+            rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->required_files = malloc(rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->total_files * sizeof(char*));
+            mem_alloc_check(rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->required_files, "rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->required_files");
 
             int i = 0;
             for(int j = 1; j < nwords; j++){
-                actionset.acts[actionset.total_actions - 1]->required_files[i] = malloc(strlen(required_files[j]) * sizeof(char) + 1);
-                mem_alloc_check(actionset.acts[actionset.total_actions - 1]->required_files[i], "actionset.acts[actionset.total_actions - 1]->required_files[i]");
-                strcpy(actionset.acts[actionset.total_actions - 1]->required_files[i], required_files[j]);
+                rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->required_files[i] = malloc(strlen(required_files[j]) * sizeof(char) + 1);
+                mem_alloc_check(rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->required_files[i], "actionset.acts[actionset.total_actions - 1]->required_files[i]");
+                strcpy(rake_file.actsets[CURR_ACTSET_INDEX]->acts[CURR_ACT_INDEX]->required_files[i], required_files[j]);
                 i++;
             }
 
@@ -219,32 +234,40 @@ int main(int argc, char const *argv[])
     }
 
     // Debug
-    printf("port = %i\n", rake_file.port);
-    for (int i = 0; i < rake_file.total_hosts; i++)
+
+    for (int i = 0; i < rake_file.total_actionsets; i++)
     {
-        printf("hosts = %s\n", rake_file.hosts[i]);
-    }
-    for (int i = 0; i < actionset.total_actions; i++)
-    {
-        printf("actionset acts commands = %s\n", actionset.acts[i]->command);
-        for (int j = 0; j < actionset.acts[i]->total_files; j++)
+        for (int j = 0; j < rake_file.actsets[i]->total_actions; j++)
         {
-            printf("actionset acts total files = %s\n", actionset.acts[i]->required_files[j]);
+            for (int x = 0; x < rake_file.actsets[i]->acts[j]->total_files; x++)
+            {
+
+            }
         }
-        
     }
-    
 
     // Clean up
     fclose(fp);
-
-    for(int i = 0; i < actionset.total_actions; i++){
-        for(int j = 0; j < actionset.acts[i]->total_files; j++){
-            free(actionset.acts[i]->required_files[j]);
+    free(rake_file_address);
+    
+    for (int i = 0; i < rake_file.total_actionsets; i++)
+    {
+        for (int j = 0; j < rake_file.actsets[i]->total_actions; j++)
+        {
+            for (int x = 0; x < rake_file.actsets[i]->acts[j]->total_files; x++)
+            {
+                free(rake_file.actsets[i]->acts[j]->required_files[x]);
+            }
+            if(rake_file.actsets[i]->acts[j]->total_files != 0){
+                free(rake_file.actsets[i]->acts[j]->required_files);
+            }
+            free(rake_file.actsets[i]->acts[j]->command);
+            free(rake_file.actsets[i]->acts[j]);
         }
-        free(actionset.acts[i]->required_files);
-        free(actionset.acts[i]->command);
+        free(rake_file.actsets[i]->acts);
+        free(rake_file.actsets[i]);
     }
+    free(rake_file.actsets);
 
     if(rake_file.hosts != NULL){
         for(int i = 0; i < rake_file.total_hosts; i++){
@@ -252,6 +275,5 @@ int main(int argc, char const *argv[])
         }
         free(rake_file.hosts);
     }
-    free(rake_file_address);
     return 0;
 }
